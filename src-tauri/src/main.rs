ffefd97c5b5cbed7c7ce9adb603727859e4a69a2
Piyush Udhao <pudhao12@gmail.com>
2024-06-09 23:22:01 +0530

    so the json structure needs to be imported into dragdropprovider but it is first accessed in TasksView

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,15 +3,10 @@
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 
-use habtrack::db::init::DbInitializer;
+use habtrack::db::{init::DbInitializer, ops};
 
 // Start work on database.
 // TODO: Once done, merge with main, and pull changes into FEATURE-add-delete-task.
-
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
 
 fn main() {
     tauri::Builder::default()
@@ -20,7 +15,11 @@ fn main() {
 
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            ops::add_task,
+            ops::add_task_group,
+            ops::get_tasks_view
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
