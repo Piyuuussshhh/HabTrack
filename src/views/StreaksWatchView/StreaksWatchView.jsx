605bd08c50936dbb8cf0275f878c1d2cbf9a3b94
Piyush Udhao <81621259+Piyuuussshhh@users.noreply.github.com>
2024-06-15 07:55:43 +0530

    fix bad import in StreaksWatchView.jsx

--- src/views/StreaksWatchView/StreaksWatchView.jsx
+++ src/views/StreaksWatchView/StreaksWatchView.jsx
@@ -1,21 +1,18 @@
-import React from 'react';
-import CalendarChart from '/home/arnax08/Documents/Piyush App/HabTrack/Cal-chart.jsx';
+import React from "react";
 
 const StreaksWatchView = () => {
-	const data = [
-			{ date: '2023-01-01', count: 1 },
-			{ date: '2023-01-02', count: 2 },
-			{ date: '2023-01-01', count: 1 },
-  			{ date: '2023-04-02', count: 9 },
+  const data = [
+    { date: "2023-01-01", count: 1 },
+    { date: "2023-01-02", count: 2 },
+    { date: "2023-01-01", count: 1 },
+    { date: "2023-04-02", count: 9 },
+  ];
 
-		  ];
-		  
-	return (
-	  <div>
-		<h1>GitHub Contributions Graph</h1>
-		<CalendarChart data={data} />
-	  </div>
-	);
-  };
-  
-  export default StreaksWatchView;
+  return (
+    <div>
+      <h1>GitHub Contributions Graph</h1>
+    </div>
+  );
+};
+
+export default StreaksWatchView;
