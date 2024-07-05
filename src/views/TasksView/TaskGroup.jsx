036b7b5cf7eb0dfe516cf6d9f4125fc5f35726bb
Piyush Udhao <pudhao12@gmail.com>
2024-07-05 17:54:29 +0530

    edit taskgroup name is a thing, also made it so that empty names cannot be submitted

--- src/views/TasksView/TaskGroup.jsx
+++ src/views/TasksView/TaskGroup.jsx
@@ -1,9 +1,10 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 
 import { IconButton } from "@mui/material";
 import { MoreVert } from "@mui/icons-material";
 import { Menu } from "@mui/material";
 import { MenuItem } from "@mui/material";
+import CheckIcon from "@mui/icons-material/Check";
 // import DeleteIcon from "@mui/icons-material/Delete";
 
 import {
@@ -12,20 +13,72 @@ import {
   TASKS_VIEW,
   TASK_GROUP,
   TAURI_DELETE_ITEM,
+  TAURI_EDIT_ITEM,
   TODAY,
 } from "../../Constants";
 import Task from "./Task";
 import { DragDropContext } from "./DragDropContext";
 import { invoke } from "@tauri-apps/api";
-import { removeItem } from "../../utility/AddRemoveUpdateItems";
+import { removeItem, updateItem } from "../../utility/AddRemoveUpdateItems";
 import AlertModal from "../../components/AlertModal";
 import { updateFrontend } from "../../utility/UpdateFrontend";
 
 const TaskGroup = ({ id, name, children, onChangeTasksView }) => {
   const [anchorEl, setAnchorEl] = useState(null);
   const [showAlert, setAlertVisibility] = useState(false);
+  const [editingTaskId, setEditingTaskId] = useState(null);
+  const [editedName, setEditedName] = useState("");
 
   const { handleOnDrop } = useContext(DragDropContext);
+
+  useEffect(() => {
+    if (editingTaskId) {
+      window.addEventListener("keydown", handleKeyDown);
+    } else {
+      window.removeEventListener("keydown", handleKeyDown);
+    }
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [editingTaskId]);
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Escape") {
+      cancelEdit();
+    }
+  };
+
+  function cancelEdit() {
+    setEditedName(null);
+    setEditingTaskId(null);
+  }
+
+  function handleEdit() {
+    if (editingTaskId) {
+      setEditingTaskId(null);
+    } else {
+      setEditingTaskId(id);
+    }
+  }
+
+  function onChange(e) {
+    setEditedName(e.target.value);
+  }
+
+  function handleConfirmEdit() {
+    // Update name in the database.
+    invoke(TAURI_EDIT_ITEM, {
+      table: TODAY,
+      name: editedName,
+      id: id,
+    });
+
+    // Update name on the frontend.
+    updateFrontend(updateItem, TASKS_VIEW, onChangeTasksView, id, editedName);
+
+    setEditingTaskId(null);
+  }
 
   const groupOptions = ["Edit", "Add Task", "Delete Group"];
 
@@ -36,6 +89,10 @@ const TaskGroup = ({ id, name, children, onChangeTasksView }) => {
   const open = Boolean(anchorEl);
 
   function handleMenuItemClick(option) {
+    if (option === "Edit") {
+      handleEdit();
+    }
+
     if (option === "Delete Group") {
       setAlertVisibility(true);
     }
@@ -76,39 +133,65 @@ const TaskGroup = ({ id, name, children, onChangeTasksView }) => {
       }}
       onDragOver={(e) => e.preventDefault()}
     >
-      {name !== ROOT && (
-        <div className="group-header">
-          {name !== ROOT && <h3 className="bold">{name}</h3>}
-          <IconButton
-            style={{ color: "white", alignSelf: "start" }}
-            aria-label="more"
-            onClick={handleClick}
-            aria-haspopup="true"
-            aria-controls="long-menu"
+      {editingTaskId === id ? (
+        <div
+          className="edit-items"
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <input
+            type="text"
+            className="edit-task-inp bold"
+            onInput={onChange}
+            placeholder="press ESC key to stop editing..."
+            autoFocus
+          />
+          <button
+            className="task-btn-tg"
+            onClick={editedName !== "" ? handleConfirmEdit : null}
           >
-            <MoreVert />
-          </IconButton>
-          {/* Use IconMenu instead for better looks. Add icon -> Add task etc.*/}
-          <Menu
-            anchorEl={anchorEl}
-            keepMounted
-            onClose={handleClose}
-            open={open}
-            MenuListProps={{
-              display: "flex",
-              "flex-direction": "column",
-            }}
-          >
-            {groupOptions.map((option) => (
-              <MenuItem
-                key={option}
-                onClick={() => handleMenuItemClick(option)}
-              >
-                {option}
-              </MenuItem>
-            ))}
-          </Menu>
+            <CheckIcon />
+          </button>
         </div>
+      ) : (
+        <>
+          {name !== ROOT && (
+            <div className="group-header">
+              {name !== ROOT && <h3 className="bold">{name}</h3>}
+              <IconButton
+                style={{ color: "white", alignSelf: "start" }}
+                aria-label="more"
+                onClick={handleClick}
+                aria-haspopup="true"
+                aria-controls="long-menu"
+              >
+                <MoreVert />
+              </IconButton>
+              <Menu
+                anchorEl={anchorEl}
+                keepMounted
+                onClose={handleClose}
+                open={open}
+                MenuListProps={{
+                  display: "flex",
+                  "flex-direction": "column",
+                }}
+              >
+                {groupOptions.map((option) => (
+                  <MenuItem
+                    key={option}
+                    onClick={() => handleMenuItemClick(option)}
+                  >
+                    {option}
+                  </MenuItem>
+                ))}
+              </Menu>
+            </div>
+          )}
+        </>
       )}
       {children.length > 0 ? (
         <div className="subtasks-list">
