26b30e465cb6db69a011400d5a6d8c7d6dd4df2c
Arnav Rajurkar <arnavrajurkar@gmail.com>
2024-07-09 15:56:16 +0530

    Intermediate commit, added a slider to change screens in help

--- src/utility/HelpScreen.jsx
+++ src/utility/HelpScreen.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 const Help = ({ isHelp, closeHelp }) => {
-    if (!isHelp) return null;
+  if (!isHelp) return null;
 
   return (
     <div className="help-screen">
@@ -12,12 +12,64 @@ const Help = ({ isHelp, closeHelp }) => {
         </button>
       </div>
 
+
+      <div className="radio-input">
+        <div className="glass">
+          <div className="glass-inner"></div>
+        </div>
+        <div className="selector">
+          <div className="choice">
+            <div className="ball1">
+              <input
+                type="radio"
+                id="one"
+                name="number-selector"
+                value="Tasks"
+                defaultChecked={true}
+                className="choice-circle"
+              />
+              <div className="ball"></div>
+            </div>
+            <label className="choice-name" htmlFor="one">
+              Task
+            </label>
+          </div>
+          <div className="choice">
+            <div className="ball2">
+              <input
+                type="radio"
+                id="two"
+                name="number-selector"
+                value="Habits"
+                className="choice-circle"
+              />
+              <div className="ball"></div>
+            </div>
+            <label className="choice-name">Habits</label>
+          </div>
+          <div className="choice">
+            <div className="ball3">
+              <input
+                type="radio"
+                id="three"
+                name="number-selector"
+                value="Streaks"
+                className="choice-circle"
+              />
+              <div className="ball"></div>
+            </div>
+            <label className="choice-name" htmlFor="three">
+              Streaks
+            </label>
+          </div>
+        </div>
+      </div>
+
+
       <div className="help-modal">
 
 
       </div>
-      
-      <div className="help-content"></div>
     </div>
   );
 };
