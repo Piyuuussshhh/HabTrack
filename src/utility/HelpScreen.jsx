b61b2ef5fce3edc49b547a556e76f524cc8dfde1
Arnav Rajurkar <arnavrajurkar@gmail.com>
2024-07-11 13:04:59 +0530

    Added carousel sliders to help screen

--- src/utility/HelpScreen.jsx
+++ src/utility/HelpScreen.jsx
@@ -1,74 +1,60 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+const PrevArrow = ({ onClick }) => (
+  <div className="custom-arrow custom-prev" onClick={onClick}>
+    <span style={{ color: "white", fontSize: "20px" }}>{"<"}</span>
+  </div>
+);
+
+const NextArrow = ({ onClick }) => (
+  <div className="custom-arrow custom-next" onClick={onClick}>
+    <span style={{ color: "white", fontSize: "20px" }}>{">"}</span>
+  </div>
+);
 
 const Help = ({ isHelp, closeHelp }) => {
+  const sliderRef = useRef(null);
+
   if (!isHelp) return null;
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false, // Hide default arrows
+  };
+
+  const slides = [
+    "Slide 1 content",
+    "Slide 2 content",
+    "Slide 3 content",
+    "Slide 4 content",
+  ];
 
   return (
     <div className="help-screen">
-      <div className="hekp-close-x-btn" onClick={closeHelp}>
+      <div className="help-close-x-btn" onClick={closeHelp}>
         <button className="help-x-btn">
           <span className="help-X"></span>
           <span className="help-Y"></span>
         </button>
       </div>
 
-
-      <div className="radio-input">
-        <div className="glass">
-          <div className="glass-inner"></div>
-        </div>
-        <div className="selector">
-          <div className="choice">
-            <div className="ball1">
-              <input
-                type="radio"
-                id="one"
-                name="number-selector"
-                value="Tasks"
-                defaultChecked={true}
-                className="choice-circle"
-              />
-              <div className="ball"></div>
-            </div>
-            <label className="choice-name" htmlFor="one">
-              Task
-            </label>
-          </div>
-          <div className="choice">
-            <div className="ball2">
-              <input
-                type="radio"
-                id="two"
-                name="number-selector"
-                value="Habits"
-                className="choice-circle"
-              />
-              <div className="ball"></div>
-            </div>
-            <label className="choice-name">Habits</label>
-          </div>
-          <div className="choice">
-            <div className="ball3">
-              <input
-                type="radio"
-                id="three"
-                name="number-selector"
-                value="Streaks"
-                className="choice-circle"
-              />
-              <div className="ball"></div>
-            </div>
-            <label className="choice-name" htmlFor="three">
-              Streaks
-            </label>
-          </div>
-        </div>
-      </div>
-
-
       <div className="help-modal">
-
-
+        <Slider ref={sliderRef} {...settings}>
+          {slides.map((slide, index) => (
+            <div key={index} className="slide">
+              {slide}
+            </div>
+          ))}
+        </Slider>
+        <PrevArrow onClick={() => sliderRef.current.slickPrev()} />
+        <NextArrow onClick={() => sliderRef.current.slickNext()} />
       </div>
     </div>
   );
